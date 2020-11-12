03352bd5db5d36b466caeae74a97e00d27c134e9
Evgenia Pintusova <jjane200012@gmail.com>
2020-11-12 12:02:17 +0300

    finished task4

---
+++ ExtraTasks/task4.1/script.js
@@ -0,0 +1,76 @@
+class Node {
+    constructor(value, next = null){
+        this.value = value;
+        this.next = next;
+    }
+}
+class List{
+    constructor(root){
+        this._length = 0;
+        this.head = null;
+        this.root = this.addNode(root);
+    }
+    addNode(value) {
+        var node = new Node(value);
+        var currentNode = this.head;
+        if (!currentNode) {
+            this.head = node;
+            this._length++;
+            return node;
+        }
+        while (currentNode.next) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = node;
+        this._length++;
+        if (length < this._length){return true;}
+        else {return false;}
+    }
+    remove(position = this._length) {
+        let currentNode = this.head;
+        let length = this._length;
+        let count = 1;
+        let beforeNodeToDelete = null;
+        let nodeToDelete = null;
+        var deletedNode = null;
+
+        if (position <=1 || position > length) {
+            return false;
+        }
+        while (count < position) {
+            beforeNodeToDelete = currentNode;
+            nodeToDelete = currentNode.next;
+            currentNode = currentNode.next;
+            count++;
+        }
+        beforeNodeToDelete.next = nodeToDelete.next;
+        deletedNode = nodeToDelete;
+        nodeToDelete = null;
+        this._length--;
+        if (length > this._length){return true;}
+        else {return false;}
+    }
+    print(){
+        let  currentNode = this.head;
+        let print = '';
+        while (currentNode) {
+            print += (currentNode.value).toString()+',\t';
+            currentNode = currentNode.next;
+        }
+        console.log(print);
+    }
+}
+
+let list = new List(5);
+console.log(list.addNode(2));
+list.addNode(54);
+list.addNode(4);
+list.addNode(94);
+list.addNode(43);
+list.addNode(14);
+list.addNode(-34);
+list.print();
+console.log(list.remove(5));
+list.print();
+
+
