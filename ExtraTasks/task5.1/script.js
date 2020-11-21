41e470d049fc10817206fea8efbb54304fe561e3
Evgenia Pintusova <jjane200012@gmail.com>
2020-11-21 19:17:11 +0300

    View classes fixed

--- ExtraTasks/task5.1/script.js
+++ ExtraTasks/task5.1/script.js
@@ -1,8 +1,7 @@
 function createCalendar(elem, year, month) {
-    let mon = month - 1; 
-    let date = new Date(year, mon);
-    document.write(`<p>Введенная дата: ${date.toLocaleDateString()}</p>`)
-    let table = `<table border="1" width="300" height="200">
+  let mon = month - 1;
+  let date = new Date(year, mon);
+  let table = `<table border="1" width="300" height="200">
                     <tr>
                         <th>пн</th>
                         <th>вт</th>
@@ -13,29 +12,29 @@ function createCalendar(elem, year, month) {
                         <th>вс</th>
                     </tr>
                     <tr>`;
-    for (let i = 0; i < getDay(date); i++) {
-        table += '<td></td>';
+  for (let i = 0; i < getDay(date); i++) {
+    table += "<td></td>";
+  }
+  while (date.getMonth() === mon) {
+    table += "<td>" + date.getDate() + "</td>";
+    if (getDay(date) % 7 === 6) {
+      table += "</tr><tr>";
     }
-    while (date.getMonth() === mon) {
-        table += '<td>' + date.getDate() + '</td>';
-        if (getDay(date) % 7 === 6) {
-          table += '</tr><tr>';
-        }
-        date.setDate(date.getDate() + 1);
+    date.setDate(date.getDate() + 1);
+  }
+  if (getDay(date) != 0) {
+    for (let i = getDay(date); i < 7; i++) {
+      table += "<td></td>";
     }
-    if (getDay(date) != 0) {
-        for (let i = getDay(date); i < 7; i++) {
-          table += '<td></td>';
-        }
-    }
-    table += '</tr></table>';
-    elem.innerHTML = table;
+  }
+  table += "</tr></table>";
+  elem.innerHTML = table;
 }
 function getDay(date) {
-    let day = date.getDay();
-    if (day === 0) {
-        day = 7;
-    }
-    return day - 1;
+  let day = date.getDay();
+  if (day === 0) {
+    day = 7;
+  }
+  return day - 1;
 }
 createCalendar(calendar, 2000, 12);
--- ExtraTasks/task5.2/script.js
+++ ExtraTasks/task5.2/script.js
@@ -1,66 +1,68 @@
-function createList(title, list){
-    let createlist = `<ul>${title}`;
-    createlist += paragraph(list);
-    createlist += `</ul>`;
-    document.getElementById("list").innerHTML = createlist;
+function createList(title, list) {
+  let createlist = `<ul>${title}`;
+  createlist += paragraph(list);
+  createlist += `</ul>`;
+  document.getElementById("list").innerHTML = createlist;
 }
-function paragraph(list){
-    let i=0;
-    let element = '';
-     while (list[i]){
-        element += `<li> ${list[i].value}`;
-        console.log(list[i].value);
-        if(list[i].children){
-            element += `<ul>`;
-            paragraph(list[i].children);
-            element += `</ul>`;
-        }
-        element += `</li>`;
-        i++;
+let element = "";
+function paragraph(list) {
+  let i = 0;
+  while (list[i]) {
+    element += `<li> ${list[i].value}`;
+    console.log(list[i].value);
+    if (list[i].children) {
+      element += `<ul>`;
+      paragraph(list[i].children);
+      element += `</ul>`;
     }
-    return element;
+    element += `</li>`;
+    i++;
+  }
+  return element;
 }
 const list = [
   {
-     value: 'Пункт 1.',
-     children: null,
+    value: "Пункт 1.",
+    children: null,
   },
   {
-     value: 'Пункт 2.',
-     children: [
-        {
-           value: 'Подпункт 2.1.',
-           children: null,
-        },
-        {
-           value: 'Подпункт 2.2.',
-           children: [
-              {
-                 value: 'Подпункт 2.2.1.',
-                 children: null,
-              },
-              {
-                 value: 'Подпункт 2.2.2.',
-                 children: null,
-              }
-           ],
-        },
-        {
-           value: 'Подпункт 2.3.',
-           children: null,
-        }
-     ]
+    value: "Пункт 2.",
+    children: [
+      {
+        value: "Подпункт 2.1.",
+        children: null,
+      },
+      {
+        value: "Подпункт 2.2.",
+        children: [
+          {
+            value: "Подпункт 2.2.1.",
+            children: null,
+          },
+          {
+            value: "Подпункт 2.2.2.",
+            children: null,
+          },
+        ],
+      },
+      {
+        value: "Подпункт 2.3.",
+        children: null,
+      },
+    ],
   },
   {
-     value: 'Пункт 3.',
-     children: null,
+    value: "Пункт 3.",
+    children: null,
   },
   {
-     value: 'Пункт 4.',
-     children: [{
-                value: 'Пункт 4.1.',
-                children: null,
-            }]
-  }
+    value: "Пункт 4.",
+    children: [
+      {
+        value: "Пункт 4.1.",
+        children: null,
+      },
+    ],
+  },
 ];
 createList("ПУНКТЫ", list);
